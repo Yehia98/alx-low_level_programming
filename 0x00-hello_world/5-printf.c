f8282cce7fffb65e7b50992a1b9b50b4761f05ea
yahyarashed <yahyarashed98@gmail.com>
2023-06-15 15:54:56 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,9 @@
  * main - printing a line using printf
  * return 0 to (success)
  */
+
 int main(void)
 {
-printf ("with proper grammer, but the outcome is a piece of art,\n")
-return(0);
+        printf("with proper grammer, but the outcome is a piece of art,\n");
+        return(0);
 }
