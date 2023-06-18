85c818c97965041f8959a9857e765d7a61a68cbc
yahyarashed <yahyarashed98@gmail.com>
2023-06-18 16:04:40 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - printing the alphabet.
+ * Description: A program that prints the alphabet.
+ * Return: Always 0 (success).
+ */
+int main(void)
+{
+	int a = 'a';
+	int A = 'A';
+
+	while (a <= 'z', A <= 'Z')
+	{
+		putchar(a);
+		putchar(A);
+		a++;
+		A++;
+	}
+return (0);
+}
