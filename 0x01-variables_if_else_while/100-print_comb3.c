0cc7a0d10cab120856420d9f9957d24a57b422b9
yahyarashed <yahyarashed98@gmail.com>
2023-06-19 06:21:55 -0700

    10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - printing all the possible combinations.
+ * Description: A program that prints all the possible combinations of numbers.
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = '0'; n < '9'; n++)
+	{
+		for (m = '0'; m < '9'; m++)
+		{
+			if (n > m || n == m)
+			{
+				++m;
+				putchar(n);
+				putchar(m);
+			}
+		}
+		if (n < '8' && m < '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+return (0);
+}
