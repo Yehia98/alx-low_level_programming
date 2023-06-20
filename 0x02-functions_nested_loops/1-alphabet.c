6a10e81a44f10bcbe3fa10fe7242c0a8728b776f
yahyarashed <yahyarashed98@gmail.com>
2023-06-20 14:55:30 -0700

    alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet.
+ * Description: A program that prints the alphabet.
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char a = 'a';
+	
+	while (a <= 'z')
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');	
+}
