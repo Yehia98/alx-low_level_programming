128c60b3cebb58d759071c33f223670fc4123cfb
yahyarashed <yahyarashed98@gmail.com>
2023-06-25 09:30:13 -0700

    multiplication; git commit -m multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers.
+ * @a : to be multiplied integres.
+ * @b : to be multiplied integres.
+ * Return (result) for the value of multiplication.
+ */
+int mul(int a, int b)
+{
+	int result = a * b;
+
+return (result);
+}
