76fb2a2f2c4f41513fe84828e42cb0d3a64db8b5
yahyarashed <yahyarashed98@gmail.com>
2023-06-30 13:03:30 -0700

    romantic

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -25,6 +25,15 @@ temp = *(n + j);
 *(n + i) = temp;
 }
 }
+/**
+ * infinite_add - add 2 numbers together
+ * @n1: text representation of 1st number to add
+ * @n2: text representation of 2nd number to add
+ * @r: pointer to buffer
+ * @size_r: buffer size
+ * Return: pointer to calling function
+ */
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 int overflow = 0, i = 0, j = 0, digits = 0;
@@ -43,11 +52,11 @@ while (j >= 0 || i >= 0 || overflow == 1)
 if (i < 0)
 val1 = 0;
 else
-val1 = *(n1 + i) - '0';
+val1 = *(n1 + i) -'0';
 if (j < 0)
 val2 = 0;
 else
-val2 = *(n2 + j) - '0';
+val2 = *(n2 + j) -'0';
 temp_tot = val1 + val2 + overflow;
 if (temp_tot >= 10)
 overflow = 1;
