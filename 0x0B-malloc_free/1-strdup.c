3f96e1921a61bdbd80a0f8bfddd78092231a52c5
yahyarashed <yahyarashed98@gmail.com>
2023-07-11 11:11:13 -0700

    ALX SE

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - duplicate to a new memory space location.
+ * @str: char
+ * Return: p
+ */
+char *_strdup(char *str)
+{
+	char *p;
+	int i, x;
+	int n = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (x = 0; str[x] != '\0'; x++)
+	{
+		n++;
+	}
+	p = malloc(sizeof(char) * n);
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+		p[i] = str[i];
+	return (p);
+}
