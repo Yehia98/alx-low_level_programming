3f17d3a5a1239977538eaf08bed94a3fa05cae17
yahyarashed <yahyarashed98@gmail.com>
2023-07-16 07:57:42 -0700

    array_range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -10,13 +10,14 @@
 int *array_range(int min, int max)
 {
 	int *ptr;
-	int i;
+	int i, size;
 
 	if (min > max)
 	{
 		return (NULL);
 	}
-	ptr = malloc(sizeof(int) * ((max - min) + 1));
+	size = (max - min) + 1;
+	ptr = malloc(sizeof(int) * size);
 	if (ptr == NULL)
 		return (NULL);
 
