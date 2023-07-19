2047029bb3e01b562b4cc18940b052cfe51e144d
yahyarashed <yahyarashed98@gmail.com>
2023-07-19 14:04:34 -0700

    a function that prints a name.

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name -  prints a name.
+ * @name: a string.
+ * @f: a function pointer that have a pramater of char pointer.
+ * Description: a function that prints a name.
+ * Return: void.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_HEADER_FILE
+#define MAIN_HEADER_FILE
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+
+#endif
+
