75f92eba75eb61c67eb2f4cbbbf8c78686650834
yahyarashed <yahyarashed98@gmail.com>
2023-07-19 15:00:33 -0700

    function points to another function to print array elemnets

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,8 @@
+#include <stddef.h>
 #ifndef MAIN_HEADER_FILE
 #define MAIN_HEADER_FILE
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
-
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
 
