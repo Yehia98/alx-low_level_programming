81331e0b0529f8b6fc00b982036f7874b5354d1e
yahyarashed <yahyarashed98@gmail.com>
2023-07-30 13:38:32 -0700

    pre main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 void pre_main(void)__attribute__((constructor));
-
+/**
+ * pre_main - executes before the main functions gets executed
+ */
 void pre_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
