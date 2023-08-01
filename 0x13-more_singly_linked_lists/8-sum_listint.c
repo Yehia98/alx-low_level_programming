3e53de0b7bf809ed5ab46147b7809ee158ff326b
yahyarashed <yahyarashed98@gmail.com>
2023-08-01 11:31:04 -0700

    the sum of all the data (n) of a listint_t linked list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * sum_listint - returns the sum of all the data (n) of a listint_t linked list
+ * @head: pointer to a linkedlist of a type of a listint_t'
+ * Return: sum of all the data (n) of a listint_t.
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint_t *temp = head;
+
+	while (temp)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+	return (sum);
+}
+
