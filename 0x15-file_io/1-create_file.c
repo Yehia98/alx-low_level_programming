1c085c87c38b256262136a9dd1f64b84d7244674
yahyarashed <yahyarashed98@gmail.com>
2023-08-08 14:29:04 -0700

    function that checks the endianness

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,18 +12,21 @@ int create_file(const char *filename, char *text_content)
 	int len = 0;
 
 	if (filename == NULL)
+	{
 		return (-1);
-	/**
-	* if (text_context == NULL)
-	*	{
-	*		for (;text_content[len]; len++)
-	*	}
-	*/
+	}
+	if (text_content != NULL)
+	{
+		for (; text_content[len];)
+			len++;
+	}
+
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(fd, text_content, len);
 	if (fd == -1 || w == -1)
+	{
 		return (-1);
-
+	}
 	close(fd);
 	return (1);
 }
