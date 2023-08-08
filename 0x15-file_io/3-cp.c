655612ea70f9befcf1720e379dfb64d3c6a5cbe0
yahyarashed <yahyarashed98@gmail.com>
2023-08-08 15:30:56 -0700

    program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -65,7 +65,8 @@ int main(int argc, char *argv[])
 	r = read(prev, buffer, 1024);
 	current = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
-	do {
+	while (r > 0)
+	{
 		if (prev == -1 || r == -1)
 		{
 			dprintf(STDERR_FILENO,
@@ -86,8 +87,7 @@ int main(int argc, char *argv[])
 		r = read(prev, buffer, 1024);
 		current = open(argv[2], O_WRONLY | O_APPEND);
 
-	} while (r > 0);
-
+	}
 	free(buffer);
 	close_fd(prev);
 	close_fd(current);
