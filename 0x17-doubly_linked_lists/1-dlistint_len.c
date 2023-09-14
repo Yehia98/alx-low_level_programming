3751b273b79371a64ea3d7b835b27402f151a175
yahyarashed <yahyarashed98@gmail.com>
2023-09-14 11:08:41 -0700

    Print list, List length

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,16 +1,18 @@
 #include "lists.h"
-/*
- *
+
+/**
+ * dlistint_len - returns the number of elements in a linked dlistint_t list.
+ * @h: address of a pointer to the head of dlistint_t list.
+ * Return: number of dlistint_t list.
  */
 size_t dlistint_len(const dlistint_t *h)
 {
 	unsigned int i = 0;
 
 	if (h == NULL)
-		return(i);
+		return (i);
 	while (h->prev != NULL)
 		h = h->prev;
-	
 	while (h != NULL)
 	{
 		i++;
